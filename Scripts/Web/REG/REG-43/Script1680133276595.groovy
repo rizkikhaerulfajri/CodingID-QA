09bf125c43c126946f7e09ecff70f1dea6ccacbc
Rizki Khaerul <rizkikhaerulfajri@gmail.com>
2023-03-30 06:48:40 +0700

    Made Test Cases for Syarat dan Ketentuan Checkbox in Register Scenario

--- Scripts/Web/REG/REG-43/Script1680133276595.groovy
+++ Scripts/Web/REG/REG-43/Script1680133276595.groovy
@@ -16,4 +16,40 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.webui.driver.DriverFactory
+import com.kms.katalon.core.exception.StepFailedException
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://demo-app.online/daftar')
+
+WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_Nama'), 'Rizki Khaerul Fajri')
+
+WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_BirthDay'), '18-Feb-1997')
+
+WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_Email'), 'Test@Example.com')
+
+WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_Whatsapp'), '081324387267')
+
+WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_KataSandi'), 'testingtesting')
+
+WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_KonfirmasiKataSandi'), 'testingtesting')
+
+WebUI.verifyElementNotChecked(findTestObject('Object Repository/Web/Register/Reg_Checkbox_SyaratDanKetentuan'), 1)
+
+WebUI.click(findTestObject('Object Repository/Web/Register/Reg_Button_Daftar'))
+
+WebUI.delay(1)
+
+WebUI.verifyMatch('https://demo-app.online/daftar', WebUI.getUrl(), false, FailureHandling.CONTINUE_ON_FAILURE)
+
+println('After URL Validation')
+
+// Call the reportValidity() function on the input element and capture the return value
+def isValid = WebUI.executeJavaScript('document.getElementById("inlineCheckbox1").reportValidity();', null)
+
+// Check that the return value is false using an assertion
+assert !isValid : 'No validation error for password field'
+
+
 

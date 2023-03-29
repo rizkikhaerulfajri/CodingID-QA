462044899f6422da04f1e16cea93884d0eaed8c3
Rizki Khaerul <rizkikhaerulfajri@gmail.com>
2023-03-29 20:43:58 +0700

    Added REG-16 with Data Driven Testing

--- Scripts/Web/REG/REG-16/Script1680060917858.groovy
+++ Scripts/Web/REG/REG-16/Script1680060917858.groovy
@@ -16,35 +16,44 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
-import com.kms.katalon.core.webui.driver.DriverFactory
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+import com.kms.katalon.core.testdata.TestDataFactory
 
 WebUI.openBrowser('')
 
-WebUI.navigateToUrl('https://demo-app.online/daftar')
+// Load test data
+def testData = TestDataFactory.findTestData("Data Files/Web/Register_Birthday")
 
-WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_Nama'), 'Rizki Khaerul Fajri')
-
-WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_BirthDay'), '18-Feb-1997')
-
-WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_Email'), 'Test@Mail.com')
-
-WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_Whatsapp'), '081324387267')
-
-WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_KataSandi'), 'testingtesting')
-
-WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_KonfirmasiKataSandi'), 'testingtesting')
-
-WebUI.check(findTestObject('Object Repository/Web/Register/Reg_Checkbox_SyaratDanKetentuan'))
-
-WebUI.click(findTestObject('Object Repository/Web/Register/Reg_Button_Daftar'))
-
-// Call the reportValidity() function on the input element and capture the return value
-def isValid = WebUI.executeJavaScript('document.getElementById("name").reportValidity();', null)
-
-// Check that the return value is false using an assertion
-assert !isValid : 'Validation error should be displayed'
-
-WebUI.delay(5)
-
-assert WebUI.getUrl() != 'https://demo-app.online/email/verify'
-
+for (def i = 1; i <= testData.getRowNumbers(); i++) {
+	def birthday = testData.getValue("Tanggal Lahir", i)
+	WebUI.navigateToUrl('https://demo-app.online/daftar')
+	
+	WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_Nama'), 'Rizki Khaerul Fajri')
+	
+	WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_BirthDay'), birthday)
+	
+	WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_Email'), 'Test@Mail.com')
+	
+	WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_Whatsapp'), '081324387267')
+	
+	WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_KataSandi'), 'testingtesting')
+	
+	WebUI.setText(findTestObject('Object Repository/Web/Register/Reg_InputText_KonfirmasiKataSandi'), 'testingtesting')
+	
+	WebUI.check(findTestObject('Object Repository/Web/Register/Reg_Checkbox_SyaratDanKetentuan'))
+	
+	WebUI.click(findTestObject('Object Repository/Web/Register/Reg_Button_Daftar'))
+	
+	WebUI.delay(5)
+	
+	// Call the reportValidity() function on the input element and capture the return value
+	if (WebUI.getUrl() == 'https://demo-app.online/daftar') {
+		def isValid = WebUI.executeJavaScript('document.getElementById("name").reportValidity();', null)
+		// Check that the return value is false using an assertion
+		assert !(isValid) : 'Validation error should be displayed'
+	}
+	
+	WebUI.delay(5)
+	
+	assert WebUI.getUrl() == 'https://demo-app.online/email/verify'
+}

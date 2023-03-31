0cd481ae4094a39d58fb1fc17be7e98ea68e98c1
Rizki Khaerul <rizkikhaerulfajri@gmail.com>
2023-03-31 16:57:05 +0700

    Fixed Data Files source to relative path

--- Scripts/Web/CP/CP-01/Script1680147538566.groovy
+++ Scripts/Web/CP/CP-01/Script1680147538566.groovy
@@ -17,3 +17,54 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://demo-app.online/login')
+
+WebUI.setText(findTestObject('Object Repository/WEB/Login/Login_inputText_Email'), 'burnerforpractice02@gmail.com')
+
+WebUI.setText(findTestObject('WEB/Login/Login_inputText_Password'), 'testingtesting')
+
+WebUI.click(findTestObject('WEB/Login/Login_button_Login'))
+
+WebUI.delay(2)
+
+WebUI.navigateToUrl('https://demo-app.online/dashboard/profile')
+
+def imgOriginal = WebUI.getAttribute(findTestObject('WEB/Profile/Profile_img_profilePhoto'), 'src')
+
+def nameOriginal = WebUI.getText(findTestObject('WEB/Profile/Profile_text_Name'))
+
+def phoneOriginal = WebUI.getText(findTestObject('WEB/Profile/Profile_text_Phone'))
+
+def birthdayOriginal = WebUI.getText(findTestObject('WEB/Profile/Profile_text_Birthday'))
+
+println(imgOriginal)
+println(nameOriginal)
+println(phoneOriginal)
+println(birthdayOriginal)
+
+WebUI.click(findTestObject('WEB/Profile/Profile_button_EditProfile'))
+
+WebUI.uploadFile(findTestObject('WEB/Change Profile/ChangeProfile_button_uploadButton'), 'D:\\Pas Photo.png')
+
+WebUI.setText(findTestObject('WEB/Change Profile/ChangeProfile_inputText_Name'), 'Edited Value')
+
+WebUI.setText(findTestObject('WEB/Change Profile/ChangeProfile_inputText_Phone'), '081123456789')
+
+WebUI.setText(findTestObject('WEB/Change Profile/ChangeProfile_inputText_BirthDay'), '01-Jan-2000')
+
+WebUI.click(findTestObject('WEB/Change Profile/ChangeProfile_button_Save'))
+
+WebUI.verifyElementText(findTestObject('Object Repository/WEB/Change Profile/Profile_p_ChangeSuccessNotif'), 'Rizki Khaerul Edited telah di edit')
+
+WebUI.click(findTestObject('Object Repository/WEB/Change Profile/Profile_button_ChangeSuccessNotif'))
+
+WebUI.verifyElementText(findTestObject('WEB/Profile/Profile_text_Name'), 'Rizki Khaerul Edited')
+
+WebUI.verifyElementText(findTestObject('WEB/Profile/Profile_text_Email'), 'burnerforpractice02@gmail.com')
+
+WebUI.verifyElementText(findTestObject('WEB/Profile/Profile_text_Phone'), '081000000000')
+
+WebUI.verifyElementText(findTestObject('WEB/Profile/Profile_text_Birthday'), '31-Dec-1923')
+
